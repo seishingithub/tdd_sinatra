7f334f904e9dd493f2594a59505ad39ad748f8a0
Peggy Griffin <seishingithub@gmail.com>
2014-03-24 15:04:40 -0600

    User can see a message on the homepage

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+  get '/' do
+    "Our Menu"
+  end
 end
--- spec/items_spec.rb
+++ spec/items_spec.rb
@@ -6,5 +6,7 @@ Capybara.app = App
 feature "managing items" do
   scenario "user manages items" do
     visit "/"
+    expect(page).to have_content("Our Menu")
+
   end
 end
