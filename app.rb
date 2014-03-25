e548c4e93a28a84405e988a491cd3c5dabdfb98f
Peggy Griffin <seishingithub@gmail.com>
2014-03-25 11:04:55 -0600

    allow user to view form page and Our Menu page

--- app.rb
+++ app.rb
@@ -2,6 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
   get '/' do
-    "Our Menu"
+    erb :index, locals: {items: ["Channa Masala", "Saag Paneer"]}
+  end
+
+  get '/add_item' do
+    erb :add_item
+  end
+
+  post '/items' do
+    redirect '/'
   end
 end
--- spec/items_spec.rb
+++ spec/items_spec.rb
@@ -8,5 +8,9 @@ feature "managing items" do
     visit "/"
     expect(page).to have_content("Our Menu")
 
+    click_link "Add Item"
+    page.fill_in 'Item Name', :with => 'Channa'
+    click_button('Add Item')
+    expect(page).to have_content("Channa")
   end
 end
